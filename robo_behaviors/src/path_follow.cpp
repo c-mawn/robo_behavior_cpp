2aa9969245b9f92d4da10822bb3e463f766f1fa7
cmawn <cmawn@olin.edu>
2025-11-03 17:33:00 -0500

    laid out file contents with everything working except path calculation logic

--- robo_behaviors/src/path_follow.cpp
+++ robo_behaviors/src/path_follow.cpp
@@ -9,12 +9,14 @@
 #include <algorithm>
 #include <numeric>
 #include <utility>
+#include <vector>
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 #include "geometry_msgs/msg/twist.hpp"
 #include "sensor_msgs/msg/laser_scan.hpp"
 #include "neato2_interfaces/msg/bump.hpp"
+#include "nav_msgs/msg/odometry.hpp"
 
 using namespace std::chrono_literals;
 
@@ -24,28 +26,80 @@ class path_follow : public rclcpp::Node{
       //create timer and publisher for controlling neato
       vel_pub_ = this->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 10);
       timer_ = this->create_wall_timer(500ms, std::bind(&path_follow::run_loop, this));
+      odom_sub_ = this->create_subscription<nav_msgs::msg::Odometry>("/odom", 10, std::bind(&path_follow::odom_callback, this, std::placeholders::_1));
     }
   
   private:
-    //define variables used throughout class
 
-    //assuming that path for neato to follow is given in this variable
-    //(can be changed in the future to be dynamically changed somehow)
-    std::vector<std::pair<float, float>> calc_vel(
-      const std::vector<std::vector<float>>& path, float dt){
+    //create a point struct to hold xy pairs
+    struct Point
+    {
+      double x;
+      double y;
+    };
+
+    //hard coded path... TODO: make dynamically changable later
+    std::vector<Point> path={
+      {0.8, 0.2},
+      {0.4, 0.4},
+      {0.6, 0.3},
+      {0.0, 0.0}
+    };
+
+    //current index of waypoint neato is at
+    size_t current_waypoint = 0;
+
+    //vars to store the current pose of neato 
+    double current_x = 0.0;
+    double current_y = 0.0;
+    double current_theta = 0.0; //in rad
+
+    /*
+    Odometry subscriber callback function that takes in the current odom of the neato
+    and updates the values in the class
+    */
+    void odom_callback(nav_msgs::msg::Odometry::SharedPtr msg){
+      //sets the positions to current odom positions
+      current_x = msg->pose.pose.position.x;
+      current_y = msg->pose.pose.position.y;
+
       /*
-      Function that takes in list of points for robot to traverse and returns
-      a vector containing pairs of linear and angular velocities to command the robot
+      Find the current theta from the odom; odom message gives quaternion angles
+      but we want euler angles. Conversion below is created by AI
       */
-     
+      double siny_cosp = 2 * (msg->pose.pose.orientation.w * msg->pose.pose.orientation.z + 
+        msg->pose.pose.orientation.x * msg->pose.pose.orientation.y);
+      double cosy_cosp = 1 - 2 * (msg->pose.pose.orientation.y * msg->pose.pose.orientation.y + 
+        msg->pose.pose.orientation.z * msg->pose.pose.orientation.z);
+      
+      //sets the current angle using the conversion above
+      current_theta = std::atan2(siny_cosp, cosy_cosp);
     }
+    
 
     void run_loop(){
+      // creates velocity message to be populated
       auto vel = geometry_msgs::msg::Twist();
 
+      RCLCPP_INFO_STREAM(this->get_logger(), "Current Pose: (x: " << current_x
+                                              << ", y: " << current_y 
+                                              << ", theta: " << current_theta << ")");
+      
+      RCLCPP_INFO_STREAM(this->get_logger(), "Target Waypoint #" << current_waypoint
+                                              << ": (x: " << path[current_waypoint].x
+                                              << ", y: " << path[current_waypoint].y << ")");
+
+      /*
+      Logic for calculating the velocity commands needs to get to the next waypoint
+      */
+
+
+      // publishes the velocity message
+      vel_pub_->publish(vel);
     }
 
     rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr vel_pub_;
+    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
     rclcpp::TimerBase::SharedPtr timer_;
 
 };
